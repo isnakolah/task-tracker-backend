cbc805a20cc8775640189c8e46c47de81ad62c94
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-20 16:58:43 +0300

    Add email field to the User email as the unique field

--- model/models.go
+++ model/models.go
@@ -4,6 +4,7 @@ import "time"
 
 type User struct {
 	Base
+	Email      string      `json:"email"`
 	FirstName  string      `json:"first_name"`
 	LastName   string      `json:"last_name"`
 	Password   string      `json:"password"`
