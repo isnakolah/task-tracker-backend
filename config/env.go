aec25ae4e2bf14e76d702e38c2134ba9020504ad
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-20 16:20:04 +0300

    Create environment variables

--- config/env.go
+++ config/env.go
@@ -2,7 +2,18 @@ package config
 
 import "os"
 
-func GetEnviron() string {
+var (
+	Environment = getEnviron()
+	DbHost      = getDatabaseHost()
+	DbPort      = getDatabasePort()
+	DbUser      = getDatabaseUser()
+	DbName      = getDatabaseUser()
+	DbPassword  = getDatabasePassword()
+	IdentityKey = getIdentityKey()
+	SecretKey   = getSecretKey()
+)
+
+func getEnviron() string {
 	env := os.Getenv("ENV")
 	if env == "" {
 		env = "DEV"
@@ -10,7 +21,7 @@ func GetEnviron() string {
 	return env
 }
 
-func GetDatabaseHost() string {
+func getDatabaseHost() string {
 	host := os.Getenv("DATABASE_HOST")
 	if host == "" {
 		host = "localhost"
@@ -18,7 +29,7 @@ func GetDatabaseHost() string {
 	return host
 }
 
-func GetDatabasePort() string {
+func getDatabasePort() string {
 	port := os.Getenv("DATABASE_HOST")
 	if port == "" {
 		port = "5432"
@@ -26,7 +37,7 @@ func GetDatabasePort() string {
 	return port
 }
 
-func GetDatabaseName() string {
+func getDatabaseName() string {
 
 	name := os.Getenv("DATABASE_NAME")
 	if name == "" {
@@ -35,7 +46,7 @@ func GetDatabaseName() string {
 	return name
 }
 
-func GetDatabaseUser() string {
+func getDatabaseUser() string {
 
 	user := os.Getenv("DATABASE_USER")
 	if user == "" {
@@ -44,11 +55,29 @@ func GetDatabaseUser() string {
 	return user
 }
 
-func GetDatabasePassword() string {
+func getDatabasePassword() string {
 
 	password := os.Getenv("DATABASE_PASSWORD")
 	if password == "" {
 		password = "postgres"
 	}
 	return password
+}
+
+func getIdentityKey() string {
+
+	identityKey := os.Getenv("IDENTITY_KEY")
+	if identityKey == "" {
+		identityKey = "id"
+	}
+	return identityKey
+}
+
+func getSecretKey() string {
+
+	secretKey := os.Getenv("SECRET_KEY")
+	if secretKey == "" {
+		secretKey = "my_secret_key_3219kd"
+	}
+	return secretKey
 }
